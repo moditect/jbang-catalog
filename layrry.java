c5e2b2d8f585e2ef6fc1a069992000d2e58002fd
Gunnar Morling <gunnar.morling@googlemail.com>
2020-10-10 22:39:05 +0200

    Defaulting to JDK 15

--- layrry.java
+++ layrry.java
@@ -1,7 +1,7 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
 //JAVA 14+
 //REPOS jitpack
-//JAVAC_OPTIONS --enable-preview --release 14
+//JAVAC_OPTIONS --enable-preview --release 15
 //JAVA_OPTIONS --enable-preview
 //DEPS com.github.moditect.layrry:layrry-launcher:master-SNAPSHOT
 
