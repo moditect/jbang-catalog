5efb73956da48289d44bd8de3cfc04d3b3f44f45
Andres Almiray <aalmiray@gmail.com>
2020-10-12 22:47:09 +0200

    Update to latest release

--- layrry.java
+++ layrry.java
@@ -1,14 +1,13 @@
 //usr/bin/env jbang "$0" "$@" ; exit $?
-//JAVA 14+
+//JAVA 15+
 //REPOS jitpack
 //JAVAC_OPTIONS --enable-preview --release 15
 //JAVA_OPTIONS --enable-preview
-//DEPS com.github.moditect.layrry:layrry-launcher:-41888424b9-1
+//DEPS com.github.moditect.layrry:layrry-launcher:master-SNAPSHOT:all
 
 import static java.lang.System.*;
 
 public class layrry {
-
     public static void main(String... args) throws Exception {
         org.moditect.layrry.launcher.LayrryLauncher.main(args);
     }
